f033eaf52019f87f447f7d19211db31533aea381
user1 <gh.user.one@gmail.com>
2023-10-29 14:17:27 +0100

    Refined code of ML methods

--- ml.py
+++ ml.py
@@ -28,86 +28,88 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-c', '--classifier', choices=['lr', 'mnb', 'svm', 'dt', 'knn', 'stck', 'rf', 'ada', 'gb', 'xgb'], help='Classification Model', required=True)
 args = parser.parse_args()
 
-# Loading the dataset in a dataframe
-data = pd.read_csv("./data/languages.csv")
+if __name__ == '__main__': 
 
-# separating the texts from the language categories 
-X = data["Text"] ; y = data["Language"]
+    # Loading the dataset in a dataframe
+    data = pd.read_csv("./data/languages.csv")
 
-# converting language categories to numerical values
-le = LabelEncoder() ; y = le.fit_transform(y)
+    # separating the texts from the language categories 
+    X = data["Text"] ; y = data["Language"]
 
-# iterating through all samples to clean them
-for i, s in enumerate(X):
-    X[i] = text_preprocess(s)
+    # converting language categories to numerical values
+    le = LabelEncoder() ; y = le.fit_transform(y)
+
+    # iterating through all samples to clean them
+    for i, s in enumerate(X):
+        X[i] = text_preprocess(s)
 
 
-'''
-Creating bag of words text representation with words used as features. A more carefull selection and combination (e.g., by including n-gram embeddings) of features can lead to better performance of the models.
-'''
-cv = CountVectorizer()
-X = cv.fit_transform(X).toarray()
+    '''
+    Creating bag of words text representation with words used as features. A more carefull selection and combination (e.g., by including n-gram embeddings) of features can lead to better performance of the models.
+    '''
+    cv = CountVectorizer()
+    X = cv.fit_transform(X).toarray()
 
-# train-test splitting of the samples
-x_train, x_test, y_train, y_test = train_test_split(X, y, test_size = 0.20)
+    # train-test splitting of the samples
+    x_train, x_test, y_train, y_test = train_test_split(X, y, test_size = 0.20)
 
-# instantiating the basic classification models
-lr_model = LogisticRegression()
-svm_model = SVC()
-mnb_model = MultinomialNB()
-knn_model = KNeighborsClassifier()
-dt_model = DecisionTreeClassifier()
+    # instantiating the basic classification models
+    lr_model = LogisticRegression()
+    svm_model = SVC()
+    mnb_model = MultinomialNB()
+    knn_model = KNeighborsClassifier()
+    dt_model = DecisionTreeClassifier()
 
-# preparing the stack ansemble model
-level0 = list() ; level0.append(('lr', lr_model)) ; level0.append(('svc', svm_model))
-level0.append(('nb', mnb_model)) ; level0.append(('knn', knn_model)) ; level0.append(('dt', dt_model))
-level1 = lr_model
-stck_model = StackingClassifier(estimators=level0, final_estimator=level1, cv=5)
+    # preparing the stack ansemble model
+    level0 = list() ; level0.append(('lr', lr_model)) ; level0.append(('svc', svm_model))
+    level0.append(('nb', mnb_model)) ; level0.append(('knn', knn_model)) ; level0.append(('dt', dt_model))
+    level1 = lr_model
+    stck_model = StackingClassifier(estimators=level0, final_estimator=level1, cv=5)
 
-# instantiating the bagging ensemble model 
-rf_model = RandomForestClassifier(random_state=7, n_jobs=-1)
+    # instantiating the bagging ensemble model 
+    rf_model = RandomForestClassifier(random_state=7, n_jobs=-1)
 
-# instantiating boosting ensemble models 
-ada_model = AdaBoostClassifier(random_state=7)
-gb_model = GradientBoostingClassifier(random_state=7)
-xgb_model = xgb.XGBClassifier(random_state=7)
+    # instantiating boosting ensemble models 
+    ada_model = AdaBoostClassifier(random_state=7)
+    gb_model = GradientBoostingClassifier(random_state=7)
+    xgb_model = xgb.XGBClassifier(random_state=7)
 
-# selecting the classifier to use in this run
-if args.classifier.lower() == "lr":
-    model = lr_model
-elif args.classifier.lower() == "mnb":
-    model = mnb_model
-elif args.classifier.lower() == "svm":
-    model = svm_model
-elif args.classifier.lower() == "knn":
-    model = knn_model
-elif args.classifier.lower() == "dt":
-    model = dt_model
-elif args.classifier.lower() == "stck":
-    model = stck_model
-elif args.classifier.lower() == "rf":
-    model = rf_model
-elif args.classifier.lower() == "ada":
-    model = ada_model
-elif args.classifier.lower() == "gb":
-    model = gb_model
-elif args.classifier.lower() == "xgb":
-    model = xgb_model
-else:
-    print("Wrong Classifier...")
-    sys.exit()
+    # selecting the classifier to use in this run
+    if args.classifier.lower() == "lr":
+        model = lr_model
+    elif args.classifier.lower() == "mnb":
+        model = mnb_model
+    elif args.classifier.lower() == "svm":
+        model = svm_model
+    elif args.classifier.lower() == "knn":
+        model = knn_model
+    elif args.classifier.lower() == "dt":
+        model = dt_model
+    elif args.classifier.lower() == "stck":
+        model = stck_model
+    elif args.classifier.lower() == "rf":
+        model = rf_model
+    elif args.classifier.lower() == "ada":
+        model = ada_model
+    elif args.classifier.lower() == "gb":
+        model = gb_model
+    elif args.classifier.lower() == "xgb":
+        model = xgb_model
+    else:
+        print("Wrong Classifier...")
+        sys.exit()
 
-# training the selected model
-model.fit(x_train, y_train)
+    # training the selected model
+    model.fit(x_train, y_train)
 
-# getting prediction and accuracy
-y_pred = model.predict(x_test)
-ac = accuracy_score(y_test, y_pred)
+    # getting prediction and accuracy
+    y_pred = model.predict(x_test)
+    ac = accuracy_score(y_test, y_pred)
 
-# report accuracy and confusion matrix
-print(f"\nOverall accuracy of the selected model is: {ac:.4f}\n")
-print(f"\nConfusion matrix is:\n {confusion_matrix(y_test, y_pred)}\n")
+    # report accuracy and confusion matrix
+    print(f"\nOverall accuracy of the selected model is: {ac:.4f}\n")
+    print(f"\nConfusion matrix is:\n {confusion_matrix(y_test, y_pred)}\n")
 
-# identifying language in text entered by the user
-target_text = input("Type or paste your text below:\n\n")
-identify(target_text)
+    # identifying language in text entered by the user
+    target_text = input("Please type or paste your text below:\n\n")
+    identify(target_text)
